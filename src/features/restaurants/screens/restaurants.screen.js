a6449621741a97b3979dcb450662ae8138d9b8a0
Bryan Salcedo <bryansalcedo@Bryans-MacBook-Air.local>
2022-04-19 15:09:18 +0800

    Added the icons

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import styled from "styled-components/native";
 import { Searchbar } from 'react-native-paper';
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component.js";
 
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled(View)`
+    padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled(View)`
+    flex: 1;
+    padding: ${(props) => props.theme.space[3]};
+`;
+
 export const RestaurantsScreen = () => (
-    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16 }}>
+    <SafeArea>
+        <SearchContainer>
             <Searchbar />
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        </SearchContainer>
+        <RestaurantListContainer>
             <RestaurantInfoCard />
-        </View>
-    </SafeAreaView>
+        </RestaurantListContainer>
+    </SafeArea>
 );
-
-const styles = StyleSheet.create({
-
-});
